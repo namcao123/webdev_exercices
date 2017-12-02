bede5be41ad8be9c05b3668dc28a52e44c7c7669
Nam Nguyen <mse.nguyennhunam@gmail.com>
2017-12-02 15:02:55 -0500

    Javascript event

---
+++ main.js
@@ -0,0 +1,28 @@
+var pageCounter = 1;
+
+
+var animalContainer = document.getElementById("animal-info");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function()
+{
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-'+ pageCounter +'.json');
+ourRequest.onload = function(){
+	var ourData = JSON.parse(ourRequest.responseText);
+	renderHTML(ourData);
+};
+ourRequest.send();
+pageCounter++;
+});
+
+function renderHTML(data) {
+  var htmlString = "";
+
+  for (i = 0; i < data.length; i++) {
+    htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+  animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+}
+
+
